44e34fd353ddb6dd1a50f4359bcf87026db41d26
JzunigaV <j_zuniga4@hotmail.com>
2019-04-26 10:47:36 -0500

    Se agrega clase

--- RiotBack/Controllers/ChampionsController.cs
+++ RiotBack/Controllers/ChampionsController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RiotSharp;
+using RiotSharp.Misc;
+using RiotBack.Models;
+using RiotSharp.Endpoints.Interfaces.Static;
 
 namespace RiotBack.Controllers
 {
@@ -12,19 +16,34 @@ namespace RiotBack.Controllers
     public class ChampionsController : ControllerBase
     {
 
-     
+
+        static RiotApi api = RiotApi.GetDevelopmentInstance("RGAPI-068b1991-d5b8-42df-8f0f-d8ce12262af2");
+       
         // GET: api/Champions
         [HttpGet]
-        public string Get()
+        public async Task<string> Get()
         {
+            
+            var championsList = new List<ChampionModel>();
 
+            try
+            {
+                var championRotation = await api.Champion.GetChampionRotationAsync(Region.Lan);
+                foreach (var champion in championRotation.FreeChampionIds)
+                {
+                    
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
 
-            asdasdasdasdasdasdasd;
-            return "Jax";
+                return ex.Message;
+            }                                
         }
 
-        // GET: api/Champions/5
-        [HttpGet("{id}", Name = "Get")]
+     // GET: api/Champions/5
+     [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
---
+++ RiotBack/Models/ChampionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiotBack.Models
+{
+    public class ChampionModel
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Abilities { get; set; }
+
+    }
+}
